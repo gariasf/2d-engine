5de739a8fb9534d3c86d0ead93d7444b16bad8f7
Guillem Arias Fauste <hello@gariasf.com>
2024-06-25 12:33:25 +0200

    test lua bindings

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,8 +4,12 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 #include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
     SDL_Init(SDL_INIT_EVERYTHING);
 
     glm::vec2 velocity = glm::vec2(5.0, -2.0);
