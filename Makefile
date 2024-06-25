1e673fdbf66db5149572d9cbaca3582f989f434b
Guillem Arias Fauste <hello@gariasf.com>
2024-06-25 12:31:48 +0200

    add lib includes to makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	g++ -Wall -std=c++17 src/*.cpp -lSDL2 -llua5.3 -o ./dist/gameengine
+	g++ -Wall -std=c++17 -I"./libs" src/*.cpp -lSDL2 -llua5.3 -o ./dist/gameengine
 run:
 	./dist/outgameengine
 clean:
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
+
+    glm::vec2 velocity = glm::vec2(5.0, -2.0);
     std::cout << "Hello, world" << std::endl;
     return 0;
 }
