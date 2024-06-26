ceef4f71fd0c73777a46fc0d517967a563eaf61c
Guillem Arias Fauste <hello@gariasf.com>
2024-06-26 18:30:04 +0200

    simple game loop skeleton

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 build:
 	g++ -Wall -std=c++17 -I"./libs" src/*.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -llua5.3 -o ./dist/gameengine
 run:
-	./dist/outgameengine
+	./dist/gameengine
 clean:
 	rm ./dist/gameengine
---
+++ src/Game.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Game
+{
+    public:
+        Game();
+        ~Game();
+        void Initialize();
+        void Run();
+        void ProcessInput();
+        void Update();
+        void Render();
+        void Destroy();
+};
