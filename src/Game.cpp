c079cb9c53a70ccaaf5feaa172a815d2c239fd4d
Guillem Arias Fauste <hello@gariasf.com>
2024-06-26 23:19:41 +0200

    display surface texture

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 Game::Game()
 {
@@ -83,10 +84,14 @@ void Game::Render() {
     SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect player = {10, 10, 20, 20};
-    SDL_RenderFillRect(renderer, &player);
+    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
 
+    SDL_Rect dstRect = {20, 20, 32, 32};
+
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+    SDL_DestroyTexture(texture);
     SDL_RenderPresent(renderer);
 }
 void Game::Run()
